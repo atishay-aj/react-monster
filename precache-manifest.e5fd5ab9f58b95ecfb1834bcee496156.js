c2be8c46e72fc4e523fd5554457a56582bbab0a8
atishay <atishayjain759@gmail.com>
2020-05-18 03:22:15 +0530

    Updates

---
+++ precache-manifest.e5fd5ab9f58b95ecfb1834bcee496156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c047a83399d1427b4e50bd972364f0bf",
+    "url": "/atishay-aj/react-monster/index.html"
+  },
+  {
+    "revision": "2e6d277d84880c98dc9c",
+    "url": "/atishay-aj/react-monster/static/css/main.aaf8a044.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/atishay-aj/react-monster/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/atishay-aj/react-monster/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6d277d84880c98dc9c",
+    "url": "/atishay-aj/react-monster/static/js/main.ee8c6469.chunk.js"
+  },
+  {
+    "revision": "eff08818dd8ec2d7fc24",
+    "url": "/atishay-aj/react-monster/static/js/runtime-main.7b54cbee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/atishay-aj/react-monster/static/media/logo.5d5d9eef.svg"
+  }
+]);
